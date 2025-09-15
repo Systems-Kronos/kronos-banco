5008259a23178ce7a852281f1c59aac5dadb7ee9
Theo Martins <theo.martins@germinare.org.br>
2025-09-14 22:16:25 -0300

    feat: Criação da procedure para criação e atualziação do DAU

---
+++ SQL/Procedures/sp_envia_sinal_vida.sql
@@ -0,0 +1,25 @@
+CREATE OR REPLACE PROCEDURE sp_envia_sinal_vida
+(
+    p_nCdUsuario BIGINT
+,   p_LocalUso   TIPO_LOCAL_USO
+,   p_dHeartbeat TIMESTAMP      DEFAULT NOW()
+)
+AS $$
+BEGIN
+    IF EXISTS(
+        SELECT 1
+          FROM RegistroDAU
+         WHERE nCdUsuario  = p_nCdUsuario
+           AND dDataSaida IS NULL
+    ) THEN
+        UPDATE RegistroDAU
+           SET dUltimoHeartbeat = p_dHeartbeat
+         WHERE nCdUsuario  = p_nCdUsuario
+           AND dDataSaida IS NULL;
+    ELSE
+        INSERT INTO RegistroDAU (nCdUsuario, dDataEntrada, cLocalUso)
+                         VALUES (p_nCdUsuario, p_dHeartbeat, p_LocalUso);
+    END IF;
+END;
+$$
+LANGUAGE plpgsql;
